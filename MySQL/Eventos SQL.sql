dff1a009ebbd5845a7628c96210c24c3215ee212
unknown <guino.rola17@gmail.com>
2025-09-17 15:15:45 -0500

    Evento vencimiento t02

---
+++ MySQL/Eventos SQL.sql
@@ -0,0 +1,15 @@
+SHOW VARIABLES LIKE 'event_scheduler';
+SET GLOBAL event_scheduler = ON;
+
+use mundo_patitas2;
+CREATE INDEX ix_orden_estado_fecha ON t02OrdenPedido (Estado, Fecha);
+
+-- Marca como Vencido cualquier pedido que siga en 'Generada' pasado el umbral
+CREATE EVENT IF NOT EXISTS ev_ordenes_vencidas
+ON SCHEDULE EVERY 10 MINUTE
+DO
+  UPDATE t02OrdenPedido
+     SET Estado = 'Vencido'
+   WHERE Estado = 'Generada'
+     AND Fecha  < NOW() - INTERVAL 24 HOUR;
+

65ad9ef011702622115c86fcfc34e22803b0b505
AS_514 <2020100944@ucss.pe>
2025-10-10 03:00:19 -0500

    Update listarIncidencias.php

--- Vista/Ajax/CUS26/listarIncidencias.php
+++ Vista/Ajax/CUS26/listarIncidencias.php
@@ -2,19 +2,7 @@
 header('Content-Type: application/json');
 include_once '../../../Controlador/CUS26Negocio.php';
 
-try {
-    $obj = new CUS26Negocio();
-    $data = $obj->listarIncidencias();
-
-    if (!empty($data)) {
-        echo json_encode(['success' => true, 'data' => $data]);
-    } else {
-        echo json_encode(['success' => false, 'message' => 'No hay incidencias registradas.']);
-    }
-} catch (Throwable $e) {
-    echo json_encode([
-        'success' => false,
-        'message' => 'Error interno: ' . $e->getMessage()
-    ]);
-}
+$obj = new CUS26Negocio();
+$data = $obj->listarIncidencias();
+echo json_encode(['success' => true, 'data' => $data]);
 ?>

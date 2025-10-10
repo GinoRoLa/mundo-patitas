e2960b91fc8b0443231d7bdea82151f8860aa0f2
AS_514 <2020100944@ucss.pe>
2025-10-10 03:04:35 -0500

    Update verIncidencias.js

--- Vista/Script/CUS26/verIncidencias.js
+++ Vista/Script/CUS26/verIncidencias.js
@@ -1,35 +1,24 @@
 $(function() {
   $('#btnVerIncidencias').on('click', function() {
     $('#tablaIncidencias').toggle();
-    const tbody = $('#tbodyIncidencias');
-    tbody.empty();
+    const tbody = $('#tbodyIncidencias').empty();
 
-    $.ajax({
-      type: 'GET',
-      url: '../Ajax/CUS26/listarIncidencias.php',
-      dataType: 'json',
-      success: function(res) {
-        if (res.success && res.data.length > 0) {
-          res.data.forEach(i => {
-            tbody.append(`
-              <tr>
-                <td>${i.IDIncidenciaEntrega}</td>
-                <td>${i.IDPedido}</td>
-                <td>${i.Cliente}</td>
-                <td>${i.Motivo}</td>
-                <td>${i.Estado}</td>
-                <td>${i.FechaIncidencia}</td>
-              </tr>
-            `);
-          });
-        } else {
-          tbody.append(`<tr><td colspan="6">${res.message || 'No hay incidencias registradas.'}</td></tr>`);
-        }
-      },
-      error: function(xhr, status, error) {
-        console.error(xhr.responseText);
-        alert('Error al cargar incidencias.');
+    $.get('../Ajax/CUS26/listarIncidencias.php', res => {
+      if (res.success && res.data.length) {
+        res.data.forEach(r => {
+          tbody.append(`
+            <tr>
+              <td>${r.IDIncidenciaEntrega}</td>
+              <td>${r.IDPedido}</td>
+              <td>${r.Cliente}</td>
+              <td>${r.Motivo}</td>
+              <td>${r.Estado}</td>
+              <td>${r.FechaIncidencia}</td>
+            </tr>`);
+        });
+      } else {
+        tbody.append('<tr><td colspan="6">No hay incidencias registradas</td></tr>');
       }
-    });
+    }, 'json');
   });
 });

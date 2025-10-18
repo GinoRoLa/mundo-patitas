53f5cdc6fbe739dcd20cd45837d4668558f72e7e
AS_514 <2020100944@ucss.pe>
2025-10-18 07:22:47 -0500

    Delete Vista/Ajax/CUS26/listarPedidos.php

--- Vista/Ajax/CUS26/listarPedidos.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../../Controlador/CUS26Negocio.php';
-header('Content-Type: application/json');
-
-$negocio = new CUS26Negocio();
-$datos = $negocio->listarPedidosPagados();
-echo json_encode($datos);

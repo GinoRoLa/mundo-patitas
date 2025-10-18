02dc92d4dacb7fa43479939a6652a30e43e8c737
AS_514 <2020100944@ucss.pe>
2025-10-18 07:27:57 -0500

    Add files via upload

---
+++ Vista/Ajax/CUS26/listarGestion.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../../../Controlador/CUS26Negocio.php';
+header('Content-Type: application/json');
+
+$negocio = new CUS26Negocio();
+echo json_encode($negocio->listarGestionNoEntregados());
+?>

853cdbec4ce04bb9abc2fe9ae5322a14927de1fa
AS_514 <2020100944@ucss.pe>
2025-11-13 02:34:59 -0500

    Delete Vista/Ajax/CUS13/listarPartidas.php

--- Vista/Ajax/CUS13/listarPartidas.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-require_once '../../../Controlador/CUS13Negocio.php';
-$neg = new CUS13Negocio();
-$data = $neg->listarPartidasActivas();
-echo json_encode($data);
-?>
